35c94476887b7c25c9edaff41d36498d5fc7bf6f
Suraj <surajshrm000@gmail.com>
2020-11-03 17:02:02 +0530

    [Suraj] Add: Calculating Fare for a Ride

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        public double CalculateFare(double distance, int time)
+        {
+            int costPerKilometer = 10;
+            int costPerMinute = 1;
+            int minimumFare = 5;
+            double totalFare = distance * costPerKilometer + time * costPerMinute;
+            if (totalFare < minimumFare)
+                return minimumFare;
+            return totalFare;
+        }
+    }
+}
