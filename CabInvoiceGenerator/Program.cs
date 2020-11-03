ab4e53870c47d9aa7214b6662a13f54eec61c5ad
Suraj <surajshrm000@gmail.com>
2020-11-03 17:32:44 +0530

    [Suraj] Add: Premium Invoice Service

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -7,6 +7,9 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
         }
     }
 }
--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -12,9 +12,9 @@ namespace CabInvoiceGeneratorTest
         [Test]
         public void GivenDistanceAndTime_CalculateFareMethodShould_ReturnTotalFare()
         {
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
             double distance = 20;
             int time = 45;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             double actualFare = invoiceGenerator.CalculateFare(distance, time);
             double expectedFare = 245;
             Assert.AreEqual(expectedFare, actualFare);
@@ -22,9 +22,9 @@ namespace CabInvoiceGeneratorTest
         [Test]
         public void GivenDistanceAndTime_CalculateFareMethodShould_ReturnMinimumFare()
         {
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
             double distance = 0.2;
             int time = 2;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             double actualFare = invoiceGenerator.CalculateFare(distance, time);
             double expectedFare = 5;
             Assert.AreEqual(expectedFare, actualFare);
@@ -41,7 +41,7 @@ namespace CabInvoiceGeneratorTest
                 new Ride(5.0, 3)
             };
             double expected = 162;
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             double actual = summary.totalFare;
             Assert.AreEqual(expected, actual);
@@ -58,7 +58,7 @@ namespace CabInvoiceGeneratorTest
                 new Ride(5.0, 3)
             };
             InvoiceSummary expected = new InvoiceSummary(5, 162);
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             Assert.AreEqual(summary, expected);
         }
@@ -74,10 +74,88 @@ namespace CabInvoiceGeneratorTest
                 new Ride(5.0, 3)
             };
             string userId = "12345";
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
             RideRepository rideRepository = new RideRepository();
             rideRepository.AddRide(userId, rides);
             Ride[] actual = rideRepository.GetRides(userId);
             Assert.AreEqual(rides, actual);
+        }
+        [Test]
+        public void GivenInvalidRideType_Should_Return_CabInvoiceException()
+        {
+            try
+            {
+                double distance = -5; //in km
+                int time = 20;   //in minute
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+                double fare = invoiceGenerator.CalculateFare(distance, time);
+            }
+            catch (CabInvoiceException ex)
+            {
+                Assert.AreEqual(ex.type, CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE);
+            }
+        }
+        [Test]
+        public void GivenInvalidDistance_Should_Return_CabInvoiceException()
+        {
+            try
+            {
+                double distance = -5; //in km
+                int time = 20;   //in minute
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+                double fare = invoiceGenerator.CalculateFare(distance, time);
+            }
+            catch (CabInvoiceException ex)
+            {
+                Assert.AreEqual(ex.type, CabInvoiceException.ExceptionType.INVALID_DISTANCE);
+            }
+        }
+        [Test]
+        public void GivenInvalidTime_Should_Return_CabInvoiceException()
+        {
+            try
+            {
+                double distance = 5; //in km
+                int time = -20;   //in minutes
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+                double fare = invoiceGenerator.CalculateFare(distance, time);
+            }
+            catch (CabInvoiceException ex)
+            {
+                Assert.AreEqual(ex.type, CabInvoiceException.ExceptionType.INVALID_TIME);
+            }
+        }
+        [Test]
+        public void GivenInvalidUserId_InvoiceServiceShould_ReturnCabInvoiceException()
+        {
+            try
+            {
+                RideRepository rideRepository = new RideRepository();
+                Ride[] actual = rideRepository.GetRides("InvalidUserID");
+            }
+            catch (CabInvoiceException ex)
+            {
+                Assert.AreEqual(ex.type, CabInvoiceException.ExceptionType.INVALID_USER_ID);
+            }
+        }
+        [Test]
+        public void GivenNullRides_InvoiceServiceShould_ReturnCabInvoiceException()
+        {
+            try
+            {
+                Ride[] rides =
+                {
+                    new Ride(5, 20),
+                    null,
+                    new Ride(2, 10)
+                };
+                RideRepository rideRepository = new RideRepository();
+                rideRepository.AddRide("111", rides);
+            }
+            catch (CabInvoiceException ex)
+            {
+                Assert.AreEqual(ex.type, CabInvoiceException.ExceptionType.NULL_RIDES);
+            }
         }
     }
 }
